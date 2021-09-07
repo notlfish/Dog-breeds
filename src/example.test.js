9715a7f606375e5b150930b302658a5c5370aa1e
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-09-06 21:22:14 -0500

    fix eslint

--- src/example.test.js
+++ src/example.test.js
@@ -1,11 +1,10 @@
-import { addItem } from './index'
+import addItem from './index';
 
 test('Add one new item to the list', () => {
-    document.body.innerHTML =
-    '<div>' +
-    '  <ul id="list"></li>' +
-    '</div>';
-    addItem();
-    const list = document.querySelectorAll('#list li');
-    expect(list).toHaveLength(1);
+  document.body.innerHTML = '<div>'
+  + '  <ul id="list"></li>'
+  + '</div>';
+  addItem();
+  const list = document.querySelectorAll('#list li');
+  expect(list).toHaveLength(1);
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-export const addItem = () => {
+const addItem = () => {
   const newItem = '<li>Mientras miro las nuevas olas</li>';
   const list = document.getElementById('list');
   list.innerHTML += newItem;
-}
+};
+
+export default addItem;
