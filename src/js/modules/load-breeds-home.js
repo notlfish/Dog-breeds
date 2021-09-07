cb4cdb0c3216827a3f3fa46564bf7c993abaa26a
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-09-07 15:45:47 -0500

    add comments button and likes

--- src/js/modules/load-breeds-home.js
+++ src/js/modules/load-breeds-home.js
@@ -1,15 +1,23 @@
 import getBreeds from './GET-breeds-home';
+import likeIcon from '../../img/heart-icon.png';
 
 const loadBreeds = async () => {
   const dogBreeds = await getBreeds();
   console.log(dogBreeds);
   const listContainer = document.querySelector('.list-container');
-  const newBreed = `<li id="${dogBreeds[0].id}">
-  <img class="breed-img" src="${dogBreeds[0].image.url}">
-  <h2>${dogBreeds[0].name}</h2>
-  </li>`;
-
-  listContainer.innerHTML += newBreed;
+  dogBreeds.forEach(e => {
+    listContainer.innerHTML += `<li id="${e.id}">
+    <img class="breed-img" src="${e.image.url}">
+    <div class="name-like-container">
+    <h2 class="names">${e.name}</h2>
+    <div class="likes-container">
+    <img class="like-icon" src="${likeIcon}">
+    <h4>0 likes</h4>
+    </div>
+    </div>
+    <button id="comment-${e.id}">Comments</button>
+    </li>`;
+  })
 
 };
 
