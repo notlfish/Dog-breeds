0203ddd3312ee4589f2f6d46f477a42ba0fb6ca3
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-09-08 11:36:54 -0300

    Add open/close popup event handlers

--- src/js/modules/load-breeds-home.js
+++ src/js/modules/load-breeds-home.js
@@ -1,21 +1,25 @@
 import getBreeds from './GET-breeds-home';
 import likeIcon from '../../img/heart-icon.png';
 
+export const KEY_PREFIX = 'breedsInfo';
+
 const loadBreeds = async () => {
   const dogBreeds = await getBreeds();
   const listContainer = document.querySelector('.list-container');
-  dogBreeds.forEach((e) => {
+  dogBreeds.forEach((breed) => {
     if (document.querySelectorAll('li').length < 21) {
-      listContainer.innerHTML += `<li id="${e.id}" class="breed-card">
-      <img class="breed-img" src="${e.image.url}">
+      breed.height = breed.height.metric;
+      localStorage.setItem(`${KEY_PREFIX}-${breed.id}`, JSON.stringify(breed));
+      listContainer.innerHTML += `<li id="${breed.id}" class="breed-card">
+      <img class="breed-img" src="${breed.image.url}">
       <div class="name-like-container">
-      <h2 class="names">${e.name}</h2>
+      <h2 class="names">${breed.name}</h2>
       <div class="likes-container">
       <img class="like-icon" src="${likeIcon}">
       <h4>0 likes</h4>
       </div>
       </div>
-      <button id="comment-${e.id}">Comments</button>
+      <button id="comment-${breed.id}" class="comment-button">Comments</button>
       </li>`;
     }
   });
